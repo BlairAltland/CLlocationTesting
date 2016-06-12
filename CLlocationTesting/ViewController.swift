f8f3ad77ecb0087ec3f06d471b2808f54634823d
BlairAltland <blairaltland@icloud.com>
2016-06-12 17:47:14 -0400

    Initial Commit
    All of the code for basic location testing

---
+++ CLlocationTesting/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  CLlocationTesting
+//
+//  Created by Blair Altland on 6/12/16.
+//  Copyright © 2016 Blairaltland. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //Labels
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    
+    //Variables
+    var locationManager: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        startLocation = nil
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0] as CLLocation!
+        
+        altitudeLabel.text = String(location.altitude)
+        latitudeLabel.text = String(location.coordinate.latitude)
+        longitudeLabel.text = String(location.coordinate.longitude)
+        
+        if startLocation == nil {
+            startLocation = location as CLLocation
+        }
+        
+    }
+
+
+}
+
